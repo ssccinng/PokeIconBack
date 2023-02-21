3eecc536fab4cc40fba718088646fdf578526d1d
scixing <whs11998@163.com>
2023-02-21 15:44:17 +0800

    添加项目文件。

---
+++ PokeIconBack/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PokeIconBack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainViewModel ViewModel { get; set; }  = new MainViewModel();
+        public string[] Files = Directory.GetFiles("img_pokei128");
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            switch (e.Key)
+            {
+                case Key.D1:
+                case Key.NumPad1:
+                    NewMethod(1);
+                    ;
+
+                    break;
+                case Key.D2:
+                case Key.NumPad2:
+                    NewMethod(2);
+
+                    break;
+                case Key.D3:
+                case Key.NumPad3:
+                    NewMethod(3);
+
+                    break;
+                case Key.D4:
+                case Key.NumPad4:
+                    NewMethod(4);
+
+                    break;
+                case Key.D5: 
+                case Key.NumPad5:
+                    NewMethod(5);
+                    break;
+
+                case Key.D6: 
+                case Key.NumPad6:
+                    NewMethod(6);
+                    break;
+                default: break;
+            }
+        }
+
+        private void NewMethod(int id)
+        {
+            var poke1 = new PokeSetting($"宝可梦{id}设定");
+            if (poke1.ShowDialog() == true)
+            {
+                var dd = $"icon{(poke1.Id + 1):0000}";
+                ViewModel.Images[id - 1] = Environment.CurrentDirectory + "/" + (Files.FirstOrDefault(s => s.StartsWith($"img_pokei128\\{dd}")) ?? "img_pokei128/icon0000_f00_s0.png");
+            }
+        }
+
+        private void Poke1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            NewMethod(int.Parse((sender as Image).Tag.ToString()));
+        }
+
+        private void heng_Checked(object sender, RoutedEventArgs e)
+        {
+            panel1.Visibility = Visibility.Visible;
+        }
+
+        private void heng_Unchecked(object sender, RoutedEventArgs e)
+        {
+            panel1.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void shu_Checked(object sender, RoutedEventArgs e)
+        {
+            panel2.Visibility = Visibility.Visible;
+        }
+
+        private void shu_Unchecked(object sender, RoutedEventArgs e)
+        {
+            panel2.Visibility = Visibility.Collapsed;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            heng.IsChecked = shu.IsChecked = true;
+        }
+    }
+
+    public class WidthToMarginConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Double width = (Double)value;
+            return new Thickness(width, 0, 0, 0);
+        }
+
+        //没有用到
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    public class HeightToMarginConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Double width = (Double)value;
+            return new Thickness(0, width, 0, 0);
+        }
+
+        //没有用到
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ PokeIconBack/PokeSetting.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PokeIconBack
+{
+    /// <summary>
+    /// PokeSetting.xaml 的交互逻辑
+    /// </summary>
+    public partial class PokeSetting : Window
+    {
+        public string Title1 { get; set; }
+
+        public PokeSetting(string title)
+        {
+            Title1 = title;
+            InitializeComponent();
+            
+        }
+        public int Id { get; set; }
+
+        private void Item_TextChanged(ModernWpf.Controls.AutoSuggestBox sender, ModernWpf.Controls.AutoSuggestBoxTextChangedEventArgs args)
+        {
+            var res = PokeTeamImageTran.TranslateHelper.PokeModels.Select(s => s.Name_Chs).Where(s => s.StartsWith(Item.Text));
+            if (res.Count() == 0 )
+            {
+                res = PokeTeamImageTran.TranslateHelper.PokeModels.Select(s => s.Name_Eng).Where(s => s.StartsWith(Item.Text, StringComparison.CurrentCultureIgnoreCase));
+                
+            }
+            else
+            {
+
+            }
+
+            Item.ItemsSource = res;
+            Id = PokeTeamImageTran.TranslateHelper.PokeModels.FindIndex(s => s.Name_Chs == Item.Text || s.Name_Eng == Item.Text);
+            aa.Text = Id.ToString();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Id = PokeTeamImageTran.TranslateHelper.PokeModels.FindIndex(s => s.Name_Chs == Item.Text || s.Name_Eng == Item.Text);
+            if (Id > 0 && Id <= 1008)
+            {
+                DialogResult = true;
+                Close();
+            }
+            else
+            {
+                Id= 0;
+                Close();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Title = Title1?? string.Empty;
+        }
+
+        private void aa_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (int.TryParse(aa.Text, out int id))
+            {
+                Id = id;
+            }
+        }
+    }
+}
