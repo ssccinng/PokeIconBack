39f52def15fba8dc586363cd06802a09693158da
scixing <whs11998@163.com>
2023-02-21 23:50:50 +0800

    历史记录

---
+++ PokeIconBack/PokeSettingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeIconBack
+{
+    public class PokeSettingViewModel: ObservableObject
+    {
+        public ObservableCollection<string> History { get; set; } = new ObservableCollection<string>();
+
+
+    }
+}
---
+++ PokeIconBack/SaveModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeIconBack
+{
+    public class SaveModel
+    {
+        public Queue<string> ImgList = new();
+
+        //public
+    }
+}
